004b789990a8fb9354b2500119483b1594b07144
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-06-17 02:59:11 +0300

    Информация об использованных элементах
    добавлены аннотации

--- TechMap/src/main/java/com/mycompany/techmap/model/UsageInfo.java
+++ TechMap/src/main/java/com/mycompany/techmap/model/UsageInfo.java
@@ -1,6 +1,8 @@
 
 package com.mycompany.techmap.model;
-
+/**
+ * Хранит информацию об использовании ресурса в конкретном изделии.
+ */
 public class UsageInfo {
     private final Product product;
     private int amount;
@@ -9,15 +11,23 @@ public class UsageInfo {
         this.product = product;
         this.amount = amount;
     }
-
+    
+    /**
+     * @return изделие
+     */
     public Product getProduct() {
         return product;
     }
-
+    /**
+     * @return количество использования
+     */
     public int getAmount() {
         return amount;
     }
-
+    /**
+     * Увеличивает количество использования.
+     * @param delta прирост
+     */
     public void addAmount(int delta) {
         this.amount += delta;
     }
