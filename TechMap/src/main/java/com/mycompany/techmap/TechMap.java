90723123746ecef213fe9152221a400e20612f6d
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-06-17 03:00:08 +0300

    main

---
+++ TechMap/src/main/java/com/mycompany/techmap/TechMap.java
@@ -0,0 +1,11 @@
+package com.mycompany.techmap;
+
+import com.mycompany.techmap.View.MainWindow;
+
+public class TechMap {
+
+    public static void main(String[] args) {
+        MainWindow fr = new MainWindow();
+        fr.setVisible(true);
+    }
+}
